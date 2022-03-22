b4f9e90a36e9ac903dfa63a9002fd558e2e54e4c
aslkw <101668112+aslkw@users.noreply.github.com>
2022-03-23 01:38:57 +0900

    taeyoon_2week_assignment

---
+++ 김태윤_2주차_과제.py
@@ -0,0 +1,60 @@
+# 문제 1번
+from ast import Sub
+
+from more_itertools import callback_iter
+
+
+def sum(a,b):
+    sum=a+b # your code
+    return sum
+
+# 문제 2번
+def sub(a,b):
+    sub=a-b # your code
+    return sub
+
+# 문제 3번
+def mul(a,b):
+    mul=a*b# your code
+    return mul
+
+# 문제 4번
+def div(a,b):
+    div=a/b# your code
+    return div
+
+# 문제 5번
+def distance(x1,y1,x2,y2):
+    distance=((x1-x2)**2+(y1-y2)**2)**(1/2)# your code
+    return distance
+
+# 문제 6번
+def short():
+    lylic = "life is short art is long"
+    OTL # your code
+    return
+
+# 문제 7번
+def myReverse(string):
+    string = "Hello"
+    myReverse=print(string[::-1])# your code
+    return myReverse
+
+# 문제 8번
+def letMeIntroduceMyself():
+     a=input("이름을 입력하시오: ")
+    b=input("취미를 입력하시오: ")
+    c=input("재학 중인 대학을 입력하시오: ")
+    d=input("생일을 입력하시오(예시: 981128): ")
+    e=d[2:4]
+    f=d[4:6]
+    letMeIntroduceMyself=print("제 이름은 'a'입니다. 제 취미는 'b' 이구요, 저는  'c' 를 다니고 있습니다. 제 생일은 'e'월 'f'일 입니다.")# your code
+    return letMeIntroduceMyself
+
+# 문제 9번
+def calcAI():
+    a=int(input("첫 번째 숫자를 입력하시오: "))
+    b=int(input("두 번째 숫자를 입력하시오: "))
+    c=a+b
+    calcAI=print(c)# your code
+    return calcAI
